14b8f9dd0993677e64bcd6be2cf2bfa0ce9ad1cc
obeltra <oscar-u2011@hotmail.com>
2024-03-06 19:03:16 -0500

    remove build
    Delete build folder

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    BATCH_SIZE: 4,
-    NAMES_API_ENDPOINT: 'http://0.0.0.0:3045/getNames',
-    PORT: '3000',
-};
